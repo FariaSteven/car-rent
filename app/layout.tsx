d08ddf650278a6321a74344c0442bc41cdd41c00
steven.faria <stevenfaria.gusmao@gmail.com>
2023-06-21 21:05:03 -0300

    starting front-end

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
-import './globals.css'
+"use client"
+
+import { ChakraProvider } from '@chakra-ui/react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ChakraProvider>
+        <body className={inter.className}>{children}</body>
+      </ChakraProvider>
     </html>
   )
 }
